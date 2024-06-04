817258707be35b01019e6999577d835dc136a26a
ª <162058354+Tairene@users.noreply.github.com>
2024-06-04 19:34:33 +0100

    Não fique parado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce;
     public float gravityModifier;
     public bool isOGround = true;
     public bool gameOver = false;
+
 
      
 
@@ -18,17 +20,20 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
        
@@ -41,10 +46,13 @@ public class PlayerController : MonoBehaviour
                 isOGround = true;
             }
             else if (Collision.gameObject.CompareTag("Obstacle"))
+
             
-            { 
-            gameOver = true;
+            {
             Debug.Log("Game Over!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         } 
     }
 }
